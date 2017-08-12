f9a618eb731d17c87f9cd3d6fd77daae5fdbd350
Mikhail <me@akkez.ru>
2017-08-12 15:52:48 +0300

    Initial commit.

---
+++ duck/local_settings.py
@@ -0,0 +1,13 @@
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'duck',
+		'USER': '',
+		'PASSWORD': '',
+		'HOST': 'localhost',
+		'PORT': '3306',
+	}
+}
